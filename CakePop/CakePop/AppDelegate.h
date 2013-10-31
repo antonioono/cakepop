221079534a126ea84c09f485f5dfe56768004582
Christina Yoon <christinayoon@Christina-Yoons-MacBook-Pro.local>
2013-10-31 18:20:11 -0400

    Initial Commit

---
+++ CakePop/CakePop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CakePop
+//
+//  Created by Christina Yoon on 10/31/13.
+//  Copyright (c) 2013 Yolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CakePop/CakePopTests/CakePopTests.m
@@ -0,0 +1,34 @@
+//
+//  CakePopTests.m
+//  CakePopTests
+//
+//  Created by Christina Yoon on 10/31/13.
+//  Copyright (c) 2013 Yolo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CakePopTests : XCTestCase
+
+@end
+
+@implementation CakePopTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
